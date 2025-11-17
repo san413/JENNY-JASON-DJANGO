c56e9b82b605d2c5cd72209d4957bf20cb3e9be0
Harendra Prajapati <harendraprajapati72@gmail.com>
2025-11-17 22:42:08 +0530

    Initial commit

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, our_story
 
 urlpatterns = [
     path('', home, name='home'),
+    path('our-story/', our_story, name='our_story'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def our_story(request):
+    return render(request, 'our_story.html')
+
